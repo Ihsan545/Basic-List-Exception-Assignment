3880daf3df6449e1f9cdb79093484f531879a4ad
anwar <Ihsan545>
2020-10-12 19:45:46 -0500

    Test passed

--- Basic_List_Exception_Assignment/test_collections/test_basic_list_exception.py
+++ Basic_List_Exception_Assignment/test_collections/test_basic_list_exception.py
@@ -5,7 +5,7 @@ import fun_with_collections.basic_list_exception as basic_list_exception
 
 class MyTestCase(unittest.TestCase):
  def test_make_list(self):
-   with patch('fun_with_collections.basic_list_exception.get_input', return_value='3'):
+   with patch('fun_with_collections.basic_list_exception.get_input', return_value='9'):
     def test_make_list(self, input):
         self.assertEqual(basic_list_exception.make_list(input), [6, 89, -9])
 
